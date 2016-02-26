f7ed2387b804f91be05f60afa06a25d7c2a158b8
Jonathan Frederic <jon.freder@gmail.com>
2016-02-25 20:50:46 -0800

    Make things work

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,34 @@
-import { spawn, shutdown } from 'enchannel-socketio-backend';
+import { spawn, shutdown, connect, disconnect } from 'enchannel-socketio-backend';
+import * as uuid from 'node-uuid';
+
 export function run() {
   const endpoint = prompt('kernel-relay endpoint:', 'http://localhost:3000/');
   const kernelName = prompt('kernel name:', 'python2');
   spawn(endpoint, kernelName).then(id => {
+    return connect(endpoint, id).then(channels => {
+      const session = uuid.v4();
+      channels.shell.next({
+        header: {
+          username: 'Anon',
+          session,
+          msg_type: 'kernel_info_request',
+          msg_id: uuid.v4(),
+          date: new Date(),
+          version: '5.0',
+        },
+        metadata: {},
+        parent_header: {},
+        content: {},
+      });
+      channels.shell.subscribe(msg => console.log(msg));
+      return new Promise(resolve => {
+        setTimeout(() => {
+          disconnect(id);
+          resolve(id);
+        }, 5000);
+      });
+    });
+  }).then(id => {
     return shutdown(endpoint, id);
   }).then(() => console.log('Done'));
 }
